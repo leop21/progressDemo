0699137323eaa0e954bc828ce74c8b46ced22634
Pandolfi <lpandolfi@hpe.com>
2017-05-25 02:58:52 -0600

    Initial Commit

---
+++ progress.js
@@ -0,0 +1,18 @@
+var progress = document.getElementById("progress");
+var progressLabel = document.querySelector("#progress-label");
+var progressSymbol = document.querySelector("#progress-symbol");
+var fullProgress = 125;
+var finalProgress = 59;
+var currentProgress = 0;
+var interval = setInterval(function() {
+	if(currentProgress <= finalProgress) {
+		currentProgress ++;
+		progress.style.width = currentProgress*100/fullProgress + "%";
+		progressSymbol.textContent = "^";
+		progressLabel.textContent = "$" + currentProgress;
+		progressSymbol.style.marginLeft = 85 + (currentProgress*100/fullProgress)*2 + "px";
+		progressLabel.style.marginLeft = 85 + (currentProgress*100/fullProgress)*2 - 10 + "px";
+		if(currentProgress == finalProgress)
+			clearInterval(interval);
+	}
+}, 50);
